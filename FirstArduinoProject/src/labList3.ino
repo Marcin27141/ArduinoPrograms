9bb71c16de771bb4a926b3be25a8e24d8c429a21
Marcin <marcin.tkocz@op.pl>
2023-10-14 22:56:25 +0200

    added lab 3 task 3

--- FirstArduinoProject/src/labList3.ino
+++ FirstArduinoProject/src/labList3.ino
@@ -11,6 +11,15 @@
 #define RED_BUTTON 2
 #define GREEN_BUTTON 4
 
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void initLCD()
+{
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+}
+
 void initRGB()
 {
     pinMode(LED_BUILTIN, OUTPUT);
@@ -91,35 +100,122 @@ void initButtons()
 
 //task 2
 //"simultaneously" changing led lights
-int leds[] = {LED_RED, LED_GREEN, LED_BLUE};
-int ledsStates[] {LOW, LOW, LOW};
-unsigned long turnTimes[] = {900UL, 1000UL, 1100UL};
-unsigned long lastChanged[] = {0UL, 0UL, 0UL};
+// int leds[] = {LED_RED, LED_GREEN, LED_BLUE};
+// int ledsStates[] {LOW, LOW, LOW};
+// unsigned long turnTimes[] = {900UL, 1000UL, 1100UL};
+// unsigned long lastChanged[] = {0UL, 0UL, 0UL};
 
-void setup()
-{
-    initRGB();
-    initButtons();
+// void setup()
+// {
+//     initRGB();
+//     initButtons();
+// }
+
+// void blinkLedIfTime(int i) {
+//   if (millis() - lastChanged[i] >= turnTimes[i])
+//   {
+//     if (ledsStates[i] == HIGH) {
+//       ledsStates[i] = LOW;
+//     }
+//     else {
+//       ledsStates[i] = HIGH;
+//     }
+//     digitalWrite(leds[i], ledsStates[i]);
+//     lastChanged[i] += turnTimes[i];
+//   }
+// }
+
+
+// void loop()
+// {
+//   for (int i = 0; i < 3; i++) {
+//     blinkLedIfTime(i);
+//   }
+// }
+
+//task 3
+//stopwatch program, green button stops/resumes, red button resets
+unsigned long lastTimeTurnedOn = 0UL;
+unsigned long timeElapsed = 0UL;
+bool stopwatchRunning = false;
+
+int getSecondsFromBeginning() {
+  return (timeElapsed + millis() - lastTimeTurnedOn)/1000;
 }
 
-void blinkLedIfTime(int i) {
-  if (millis() - lastChanged[i] >= turnTimes[i])
-  {
-    if (ledsStates[i] == HIGH) {
-      ledsStates[i] = LOW;
+void printMillisFromStart() {
+  lcd.setCursor(0, 1);
+  lcd.print(getSecondsFromBeginning());
+}
+
+#define DEBOUNCE_PERIOD 10UL
+int buttons[] = {GREEN_BUTTON, RED_BUTTON};
+int debouncedStates[] = {HIGH, HIGH};
+int previousReadings[] = {HIGH, HIGH};
+unsigned long lastChanged[] = {0UL, 0UL};
+
+bool isButtonPressed(int buttonId) {
+    bool isPressed = false;
+    int current_reading = digitalRead(buttons[buttonId]);
+
+    if (previousReadings[buttonId] != current_reading)
+    {
+        lastChanged[buttonId] = millis();
     }
-    else {
-      ledsStates[i] = HIGH;
+
+    if (millis() - lastChanged[buttonId] > DEBOUNCE_PERIOD)
+    {
+        if(current_reading != debouncedStates[buttonId])
+        {
+            if (debouncedStates[buttonId] == HIGH && current_reading == LOW)
+            {
+                isPressed = true;
+            }
+            debouncedStates[buttonId] = current_reading;
+        }
     }
-    digitalWrite(leds[i], ledsStates[i]);
-    lastChanged[i] += turnTimes[i];
+
+    previousReadings[buttonId] = current_reading;
+    return isPressed;
+}
+
+void switchTheStopwatchState() {
+  if (stopwatchRunning == false) {
+    lastTimeTurnedOn = millis();
+    stopwatchRunning = true;
+  }
+  else {
+    timeElapsed += (millis() - lastTimeTurnedOn);
+    stopwatchRunning = false;
   }
 }
 
+void resetStopwatch() {
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
+  stopwatchRunning = false;
+  lastTimeTurnedOn = 0UL;
+  timeElapsed = 0UL;
+}
 
-void loop()
-{
-  for (int i = 0; i < 3; i++) {
-    blinkLedIfTime(i);
+void setup() {
+  initLCD();
+  initButtons();
+
+  lcd.print("Time elapsed:");
+}
+
+int greenButtonId = 0;
+int redButtonId = 1;
+
+void loop() {
+  if (isButtonPressed(greenButtonId)) {
+    switchTheStopwatchState();
   }
+  if (isButtonPressed(redButtonId)) {
+    resetStopwatch();
+  }
+  if (stopwatchRunning == true) {
+    printMillisFromStart();
+  }  
 }
