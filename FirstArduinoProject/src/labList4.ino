a0e3ed0d3ca3ecbbdb07e5dcdb44a5075109bf59
Marcin <marcin.tkocz@op.pl>
2023-10-20 21:10:33 +0200

    lab 4 task 3

--- FirstArduinoProject/src/labList4.ino
+++ FirstArduinoProject/src/labList4.ino
@@ -88,79 +88,120 @@ void initButtons()
 
 //task 2
 //light RGB led colors based on Serial input
-bool isValidNumber(String input) {
-  int result = input.toInt();
-  return input == "0" || (result > 0 && result <= 255);
+// bool isValidNumber(String input) {
+//   int result = input.toInt();
+//   return input == "0" || (result > 0 && result <= 255);
+// }
+
+// bool validatePassedValues(String r, String g, String b) {
+//   String notValidMessage = " is not a valid number. Put a number from 0 to 255";
+
+//   if (!isValidNumber(r)) {
+//     Serial.println(r + notValidMessage);
+//     return false;
+//   }
+//   if (!isValidNumber(g)) {
+//     Serial.println(g + notValidMessage);
+//     return false;
+//   }
+//   if (!isValidNumber(b)) {
+//     Serial.println(b + notValidMessage);
+//     return false;
+//   }
+//   return true;
+// }
+
+// void printSuccessfulCommand(int r_intensity, int g_intensity, int b_intensity) {
+//   Serial.print(r_intensity);
+//   Serial.print(" ");
+//   Serial.print(g_intensity);
+//   Serial.print(" ");
+//   Serial.println(b_intensity);
+// }
+
+// void setup()
+// {
+//     initRGB();
+//     Serial.begin(9600);
+//     while (!Serial)
+//     {
+//     }
+// }
+
+// //message format: red_percent, green_percent, blue_percent
+// void loop()
+// {
+//     if (Serial.available() > 0)
+//     {
+//         String command = Serial.readStringUntil('\n');
+//         command.trim();
+//         int space1 = command.indexOf(' ');
+//         int space2 = command.indexOf(' ', space1 + 1);
+//         int space3 = command.indexOf(' ', space2 + 1);
+
+//         if (space1 == -1 || space2 == -1 || space3 != -1) {
+//           Serial.println("Incorrect line format. Separate 3 intensity values with single spaces");
+//         }
+//         else {
+//           String r_str = command.substring(0, space1);
+//           String g_str = command.substring(space1 + 1, space2);
+//           String b_str = command.substring(space2 + 1);
+
+//           bool valuesAreValid = validatePassedValues(r_str, g_str, b_str);
+//           if (valuesAreValid) {
+//             int r_intensity = r_str.toInt();
+//             int g_intensity = g_str.toInt();
+//             int b_intensity = b_str.toInt();
+
+//             analogWrite(LED_RED, r_intensity);
+//             analogWrite(LED_GREEN, g_intensity);
+//             analogWrite(LED_BLUE, b_intensity);
+//             printSuccessfulCommand(r_intensity, g_intensity, b_intensity);
+//           }
+//         }
+
+        
+//     }
+// }
+
+//zadanie 3
+//showing on lcd potentiometer real and read value
+
+void printValuesOnLcd(int sensor, float real) {
+  lcd.setCursor(12, 0);
+  char paddedSensorValue[5];
+  sprintf(paddedSensorValue, "%4d", sensor);
+  lcd.print(paddedSensorValue);
+
+  lcd.setCursor(12, 1);
+  char paddedRealValue[10];
+  dtostrf(real, 3, 2, paddedRealValue);
+  lcd.print(paddedRealValue);
 }
 
-bool validatePassedValues(String r, String g, String b) {
-  String notValidMessage = " is not a valid number. Put a number from 0 to 255";
 
-  if (!isValidNumber(r)) {
-    Serial.println(r + notValidMessage);
-    return false;
-  }
-  if (!isValidNumber(g)) {
-    Serial.println(g + notValidMessage);
-    return false;
-  }
-  if (!isValidNumber(b)) {
-    Serial.println(b + notValidMessage);
-    return false;
-  }
-  return true;
-}
-
-void printSuccessfulCommand(int r_intensity, int g_intensity, int b_intensity) {
-  Serial.print(r_intensity);
-  Serial.print(" ");
-  Serial.print(g_intensity);
-  Serial.print(" ");
-  Serial.println(b_intensity);
-}
 
 void setup()
 {
-    initRGB();
-    Serial.begin(9600);
-    while (!Serial)
-    {
-    }
+  initLCD();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Sensor:");
+  lcd.setCursor(0, 1);
+  lcd.print("Real:");
+
+  Serial.begin(9600);
 }
 
-//message format: red_percent, green_percent, blue_percent
+#define REF_VOLTAGE 5.0
+#define RESOLUTION 1024
+
+int sensorValue = 0; 
+float sensorRealValue = 0;
+
 void loop()
 {
-    if (Serial.available() > 0)
-    {
-        String command = Serial.readStringUntil('\n');
-        command.trim();
-        int space1 = command.indexOf(' ');
-        int space2 = command.indexOf(' ', space1 + 1);
-        int space3 = command.indexOf(' ', space2 + 1);
-
-        if (space1 == -1 || space2 == -1 || space3 != -1) {
-          Serial.println("Incorrect line format. Separate 3 intensity values with single spaces");
-        }
-        else {
-          String r_str = command.substring(0, space1);
-          String g_str = command.substring(space1 + 1, space2);
-          String b_str = command.substring(space2 + 1);
-
-          bool valuesAreValid = validatePassedValues(r_str, g_str, b_str);
-          if (valuesAreValid) {
-            int r_intensity = r_str.toInt();
-            int g_intensity = g_str.toInt();
-            int b_intensity = b_str.toInt();
-
-            analogWrite(LED_RED, r_intensity);
-            analogWrite(LED_GREEN, g_intensity);
-            analogWrite(LED_BLUE, b_intensity);
-            printSuccessfulCommand(r_intensity, g_intensity, b_intensity);
-          }
-        }
-
-        
-    }
+    sensorValue = analogRead(POTENTIOMETER);
+    sensorRealValue = REF_VOLTAGE / RESOLUTION * sensorValue;
+    printValuesOnLcd(sensorValue, sensorRealValue);
 }
-
