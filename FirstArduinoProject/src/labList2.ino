5e1ca03486515e83ef738a2d13d150a89f95dfa6
Marcin27141 <marcin.tkocz@op.pl>
2023-10-30 14:07:04 +0100

    refactored lab2 taks 1

--- FirstArduinoProject/src/labList2.ino
+++ FirstArduinoProject/src/labList2.ino
@@ -1,239 +1,201 @@
-// #include <Initializer.h>
+#include <Initializer.h>
 
-// //#include <Arduino.h>
-// // #include <LiquidCrystal_I2C.h>
-// // #include <Wire.h>
-// // #include <OneWire.h>
-// // #include <DallasTemperature.h>
+//----------------------task1------------------------
+//green button changes color, red button turns on/off
+// int buttons[] = {GREEN_BUTTON, RED_BUTTON};
+// int buttonsStates[] = {HIGH, HIGH};
+int leds[] = {LED_RED, LED_GREEN, LED_BLUE};
+const int LED_RED_INDEX = 0;
+const int LED_GREEN_INDEX = 1;
+const int LED_BLUE_INDEX = 2;
+int currectLedOn;
+bool turnedOn;
 
-// // LiquidCrystal_I2C lcd(0x27,16,2);
+#define DEBOUNCE_PERIOD 10UL
+int buttons[] = {GREEN_BUTTON, RED_BUTTON};
+const int GREEN_BUTTON_INDEX = 0;
+const int RED_BUTTON_INDEX = 1;
+int debouncedStates[] = {HIGH, HIGH};
+int previousReadings[] = {HIGH, HIGH};
+unsigned long lastChanged[] = {0UL, 0UL};
 
-// // OneWire oneWire(A1);
-// // DallasTemperature sensors(&oneWire);
+void initializeForTask1(LiquidCrystal_I2C* lcd) {
+  currectLedOn = LED_RED_INDEX;
+  showColor();
 
-// // #define POTENTIOMETER A0
+  lcd->setCursor(0, 0);
+  lcd->print("GREEN: COLOR");
+  lcd->setCursor(0, 1);
+  lcd->print("RED: TURN ON/OFF");
+}
 
-// // #define LED_RED 6
-// // #define LED_GREEN 5
-// // #define LED_BLUE 3
+bool isButtonPressed(int buttonId) {
+    bool isPressed = false;
+    int current_reading = digitalRead(buttons[buttonId]);
 
-// // #define RED_BUTTON 2
-// // #define GREEN_BUTTON 4
+    if (previousReadings[buttonId] != current_reading)
+    {
+        lastChanged[buttonId] = millis();
+    }
 
-// // #define ENCODER1 A2
-// // #define ENCODER2 A3
+    if (millis() - lastChanged[buttonId] > DEBOUNCE_PERIOD)
+    {
+        if(current_reading != debouncedStates[buttonId])
+        {
+            if (debouncedStates[buttonId] == HIGH && current_reading == LOW)
+            {
+                isPressed = true;
+            }
+            debouncedStates[buttonId] = current_reading;
+        }
+    }
 
-// // volatile bool redPressed = false;
+    previousReadings[buttonId] = current_reading;
+    return isPressed;
+}
 
+void runTask1() {
+  int delayTime = 400;
+  
+  delay(delayTime);
 
-// // void initEncoder()
-// // {
-// //   pinMode(ENCODER1, INPUT_PULLUP);
-// //   pinMode(ENCODER2, INPUT_PULLUP);
-// // }
+  while (true)
+  {
+    if (isButtonPressed(RED_BUTTON_INDEX)) {
+        turnLeds();
+    }
+    if (isButtonPressed(GREEN_BUTTON_INDEX)) {
+        switchColor();
+        if (turnedOn)
+            showColor();
+    }
+  }
+}
 
-// // void initLCD()
-// // {
-// //   lcd.init();
-// //   lcd.clear();
-// //   lcd.backlight();
-// // }
+void switchColor() {
+  currectLedOn = (currectLedOn + 1) % 3;
+}
 
-// // void initRGB()
-// // {
+void showColor() {
+    for (int i = 0; i < 3; i++) {
+        int state = i == currectLedOn ? HIGH : LOW;
+        digitalWrite(leds[i], state);
+    }     
+}
 
-// //   pinMode(LED_RED, OUTPUT);
-// //   pinMode(LED_GREEN, OUTPUT);
-// //   pinMode(LED_BLUE, OUTPUT);
-// // }
+void turnLedsOff() {
+    for (int i = 0; i < 3; i++) {
+        digitalWrite(leds[i], LOW);
+    }     
+}
 
-// // void initButtons()
-// // {
-// //   pinMode(RED_BUTTON, INPUT_PULLUP);
-// //   pinMode(GREEN_BUTTON, INPUT_PULLUP);
-// // }
+void turnLeds() {
+  if (turnedOn == true) {
+    turnLedsOff();
+    turnedOn = false;
+  } else {
+    showColor();
+    turnedOn = true;
+  }
+}
 
-// // void initSensors()
-// // {
-// //   sensors.begin();
-// // }
+void setup() {
+  Initializer initializer = Initializer();
+  initializer.initRGB();
+  initializer.initButtons();
+  initializer.initLCD();
+  LiquidCrystal_I2C* lcd = initializer.getLCD();
 
-// //----------------------task1------------------------
-// //green button changes color, red button turns on/off
+  initializeForTask1(lcd);
+  Serial.begin(9600);
+  runTask1();
+}
 
-// int greenBtn = HIGH;
-// int redBtn = HIGH;
-// bool turnedOn = true;
-// int redState = HIGH;
-// int greenState = LOW;
-// int blueState = LOW;
+void loop() {
+}
 
-// void initializeForTask1(Initializer initializer) {
-//   initializer.initLCD();
-//   LiquidCrystal_I2C lcd = initializer.getLCD();
-//   digitalWrite(LED_RED, HIGH);
-//   digitalWrite(LED_GREEN, LOW);
-//   digitalWrite(LED_BLUE, LOW);
-//   lcd.setCursor(0, 0);
-//   lcd.print("GREEN: COLOR");
+//------------------------task2-----------------------------
+//LED PWM presentation - red button makes led darker, green button - brighter
+
+// void initializeForTask2(int analogVal) {
+//   lcd.clear();
+//   lcd.print("GREEN: BRIGHTER");
 //   lcd.setCursor(0, 1);
-//   lcd.print("RED: TURN ON/OFF");
+//   lcd.print("RED: DARKER");
+
+//   analogWrite(LED_GREEN, analogVal);
 // }
 
-// void runTask1() {
-//   int delayTime = 400;
+// void runTask2() {
+//   int analogVal = 150;
+//   int redBtn = HIGH;
+//   int greenBtn = HIGH;
+//   initializeForTask2(analogVal);
   
-//   delay(delayTime);
-
-//   while (true)
-//   {
-//     redBtn = digitalRead(RED_BUTTON);
-//     if (redBtn == LOW) {
-//       turnLeds();
-//       delay(delayTime);
+//   while (true) {
+//     greenBtn = digitalRead(GREEN_BUTTON);
+//     if (greenBtn == LOW && analogVal < (255 - 10)) {
+//       analogVal = analogVal + 10;
 //     }
 
+//     redBtn = digitalRead(RED_BUTTON);
+//     if (redBtn == LOW && analogVal > (0 + 10)) {
+//       analogVal = analogVal - 10;
+//     }
+//     analogWrite(LED_GREEN, analogVal);
+//     delay(200);
+//   }
+// }
+
+// //------------------------task3-----------------------------
+// //on green button press, led fluently changes colors: red, green, blue, red, ... 
+
+// int singleStep = 255/100;
+// int redAnalogVal = 255;
+// int greenAnalogVal = 0;
+// int blueAnalogVal = 0;
+
+// void initializeForTask3() {
+//   lcd.clear();
+//   lcd.print("PRESS GREEN TO");
+//   lcd.setCursor(0, 1);
+//   lcd.print("CHANGE COLOR");
+
+//   analogWrite(LED_RED, redAnalogVal);
+// }
+
+// void runTask3() {
+//   int greenBtn = HIGH;
+//   initializeForTask3();
+  
+//   while (true) {
 //     greenBtn = digitalRead(GREEN_BUTTON);
 //     if (greenBtn == LOW) {
-//       switchColor();
-//       if (turnedOn)
-//         showColor();
-//       delay(delayTime);
+//       changeColorIntensity();
+//       showUpdatedIntensity();
 //     }
-//   }
-// }
-
-// void switchColor() {
-//   if (redState == HIGH) {
-//     redState = LOW;
-//     greenState = HIGH;
-//   }
-//   else if (greenState == HIGH) {
-//     greenState = LOW;
-//     blueState = HIGH;
-//   }
-//   else if (blueState == HIGH) {
-//     blueState = LOW;
-//     redState = HIGH;
-//   }
-// }
-
-// void showColor() {
-//   digitalWrite(LED_RED, redState);
-//   digitalWrite(LED_GREEN, greenState);
-//   digitalWrite(LED_BLUE, blueState);
-// }
-
-// void turnLeds() {
-//   if (turnedOn == true) {
-//     digitalWrite(LED_RED, LOW);
-//     digitalWrite(LED_GREEN, LOW);
-//     digitalWrite(LED_BLUE, LOW);
-//     turnedOn = false;
-//   } else {
-//     digitalWrite(LED_RED, redState);
-//     digitalWrite(LED_GREEN, greenState);
-//     digitalWrite(LED_BLUE, blueState);
-//     turnedOn = true;
-//   }
-// }
-
-// void setup() {
-//   Initializer initializer = Initializer();
-//   initializer.initRGB();
-//   initializer.initButtons();
-
-//   initializeForTask1(initializer);
-//   runTask1();
-// }
-
-// void loop() {
-// }
-
-// //------------------------task2-----------------------------
-// //LED PWM presentation - red button makes led darker, green button - brighter
-
-// // void initializeForTask2(int analogVal) {
-// //   lcd.clear();
-// //   lcd.print("GREEN: BRIGHTER");
-// //   lcd.setCursor(0, 1);
-// //   lcd.print("RED: DARKER");
-
-// //   analogWrite(LED_GREEN, analogVal);
-// // }
-
-// // void runTask2() {
-// //   int analogVal = 150;
-// //   int redBtn = HIGH;
-// //   int greenBtn = HIGH;
-// //   initializeForTask2(analogVal);
-  
-// //   while (true) {
-// //     greenBtn = digitalRead(GREEN_BUTTON);
-// //     if (greenBtn == LOW && analogVal < (255 - 10)) {
-// //       analogVal = analogVal + 10;
-// //     }
-
-// //     redBtn = digitalRead(RED_BUTTON);
-// //     if (redBtn == LOW && analogVal > (0 + 10)) {
-// //       analogVal = analogVal - 10;
-// //     }
-// //     analogWrite(LED_GREEN, analogVal);
-// //     delay(200);
-// //   }
-// // }
-
-// // //------------------------task3-----------------------------
-// // //on green button press, led fluently changes colors: red, green, blue, red, ... 
-
-// // int singleStep = 255/100;
-// // int redAnalogVal = 255;
-// // int greenAnalogVal = 0;
-// // int blueAnalogVal = 0;
-
-// // void initializeForTask3() {
-// //   lcd.clear();
-// //   lcd.print("PRESS GREEN TO");
-// //   lcd.setCursor(0, 1);
-// //   lcd.print("CHANGE COLOR");
-
-// //   analogWrite(LED_RED, redAnalogVal);
-// // }
-
-// // void runTask3() {
-// //   int greenBtn = HIGH;
-// //   initializeForTask3();
-  
-// //   while (true) {
-// //     greenBtn = digitalRead(GREEN_BUTTON);
-// //     if (greenBtn == LOW) {
-// //       changeColorIntensity();
-// //       showUpdatedIntensity();
-// //     }
     
-// //     delay(45);
-// //   }
-// // }
+//     delay(45);
+//   }
+// }
 
-// // void changeColorIntensity() {
-// //   if (redAnalogVal > singleStep && blueAnalogVal <= singleStep) {
-// //     redAnalogVal -= singleStep;
-// //     greenAnalogVal += singleStep;
-// //   }
-// //   else if (greenAnalogVal > singleStep) {
-// //     greenAnalogVal -= singleStep;
-// //     blueAnalogVal += singleStep;
-// //   }
-// //   else if (blueAnalogVal > singleStep) {
-// //     blueAnalogVal -= singleStep;
-// //     redAnalogVal += singleStep;
-// //   }
-// // }
+// void changeColorIntensity() {
+//   if (redAnalogVal > singleStep && blueAnalogVal <= singleStep) {
+//     redAnalogVal -= singleStep;
+//     greenAnalogVal += singleStep;
+//   }
+//   else if (greenAnalogVal > singleStep) {
+//     greenAnalogVal -= singleStep;
+//     blueAnalogVal += singleStep;
+//   }
+//   else if (blueAnalogVal > singleStep) {
+//     blueAnalogVal -= singleStep;
+//     redAnalogVal += singleStep;
+//   }
+// }
 
-// // void showUpdatedIntensity() {
-// //   analogWrite(LED_RED, redAnalogVal);
-// //   analogWrite(LED_GREEN, greenAnalogVal);
-// //   analogWrite(LED_BLUE, blueAnalogVal);
-// // }
-
-// // //--------------------------main------------------
+// void showUpdatedIntensity() {
+//   analogWrite(LED_RED, redAnalogVal);
+//   analogWrite(LED_GREEN, greenAnalogVal);
+//   analogWrite(LED_BLUE, blueAnalogVal);
+// }
