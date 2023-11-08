0bdbe2ade40043d5ea51a54ee6f142fbfc23611b
Marcin27141 <marcin.tkocz@op.pl>
2023-11-08 10:24:53 +0100

    setting color intensity with encoder menu

--- FirstArduinoProject/src/labList5.ino
+++ FirstArduinoProject/src/labList5.ino
@@ -1,205 +1,264 @@
-// #include <Arduino.h>
-// #include <LiquidCrystal_I2C.h>
-// #include <util/atomic.h>
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include <util/atomic.h>
+#include <Initializer.h>
 
-// LiquidCrystal_I2C lcd(0x27,16,2);
+LiquidCrystal_I2C lcd(0x27,16,2);
 
-// #define LED_RED 6
-// #define LED_GREEN 5
-// #define LED_BLUE 3
+void initLCD()
+{
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+}
 
-// #define RED_BUTTON 2
-// #define GREEN_BUTTON 4
+void initRGB()
+{
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+}
 
-// #define ENCODER1 A2
-// #define ENCODER2 A3
+void initButtons()
+{
+  pinMode(RED_BUTTON, INPUT_PULLUP);
+  pinMode(GREEN_BUTTON, INPUT_PULLUP);
+}
 
-// void initLCD()
-// {
-//   lcd.init();
-//   lcd.clear();
-//   lcd.backlight();
-// }
+void initEncoder()
+{
+  pinMode(ENCODER1, INPUT_PULLUP);
+  pinMode(ENCODER2, INPUT_PULLUP);
+}
 
-// void initRGB()
-// {
+#define BUTTON_DEBOUNCE_PERIOD 100
+#define DEBOUNCING_PERIOD 100
+bool isScrollingMenu = true;
 
-//   pinMode(LED_RED, OUTPUT);
-//   pinMode(LED_GREEN, OUTPUT);
-//   pinMode(LED_BLUE, OUTPUT);
-// }
+int colorIntensities[] = { 0, 0, 0 };
+int colors[] = { LED_RED, LED_GREEN, LED_BLUE };
 
-// void initButtons()
-// {
-//   pinMode(RED_BUTTON, INPUT_PULLUP);
-//   pinMode(GREEN_BUTTON, INPUT_PULLUP);
-// }
+String getColorName(int colorIdx) {
+  switch (colorIdx) {
+    case 0:
+      return "RED";
+    case 1:
+      return "GREEN";
+    case 2:
+      return "BLUE";
+    default:
+      return "unknown";
+  }
+}
 
-// void initEncoder()
-// {
-//   pinMode(ENCODER1, INPUT_PULLUP);
-//   pinMode(ENCODER2, INPUT_PULLUP);
-// }
+String menuOptions[] = {
+  "LED Red",
+  "LED Green",
+  "LED Blue",
+};
+int numberOfMenuOptions = sizeof(menuOptions) / sizeof(menuOptions[0]);
 
-// #define BUTTON_DEBOUNCE_PERIOD 100
+int menuVisibleItem = 0;
 
-// String menuOptions[] = {
-//   "LED Red",
-//   "LED Green",
-//   "LED Blue",
-//   "LED White",
-//   "LED None"
-// };
-// int numberOfMenuOptions = sizeof(menuOptions) / sizeof(menuOptions[0]);
+void printMenuTitle() {
+    char buffer[17];
+    String title = "Sterowanie RGB";
+    sprintf(buffer, "%-14s", title.c_str());
+    lcd.setCursor(0, 0);
+    lcd.print(buffer);
+}
 
-// int menuTopItem = 0;
+int getPercentOfIntensity(int intensity) {
+    return intensity * 100 / 255;
+}
 
-// void printMenu() {
-//     char fstBuffer[17];
-//     sprintf(fstBuffer, ">>%-14s", menuOptions[menuTopItem].c_str());
-//     lcd.setCursor(0, 0);
-//     lcd.print(fstBuffer);
+String getMenuOptionText(int optionIdx) {
+    return menuOptions[optionIdx] + " " + getPercentOfIntensity(colorIntensities[optionIdx]) + "%";
+}
 
-//     int nextItem = (menuTopItem + 1) % numberOfMenuOptions;
+void printScrollableMenuOption() {
+    String menuOption = getMenuOptionText(menuVisibleItem);
+    char buffer[17];
+    sprintf(buffer, "%-16s", menuOption.c_str());
+    lcd.setCursor(0, 1);
+    lcd.print(buffer);
+}
 
-//     char sndBuffer[17];
-//     sprintf(sndBuffer, "%-16s", menuOptions[nextItem].c_str());
-//     lcd.setCursor(0, 1);
-//     lcd.print(sndBuffer);
-// }
+void printMenu() {
+    printMenuTitle();
+    printScrollableMenuOption();
+}
 
-// void printResults(int val)
-// {
-//     char buffer[40];
-//     sprintf(buffer, "Encoder: %3d", val);
-//     lcd.setCursor(2, 0);
-//     lcd.print(buffer);
-// }
+volatile int encoder1 = HIGH;
+volatile int encoder2 = HIGH;
+volatile unsigned long encoderTimestamp = 0UL;
 
-// void setup()
-// {
-//   initButtons();
-//     initRGB();
-//     initEncoder();
-//     initLCD();
+ISR(PCINT1_vect)
+{
+    encoder1 = digitalRead(ENCODER1);
+    encoder2 = digitalRead(ENCODER2);
+    encoderTimestamp = millis();
+}
 
-//     printMenu();
+int encoderValue = 0;
+int lastEn1 = LOW;
+unsigned long lastChangeTimestamp = 0UL;
 
-//     PCICR |= (1 << PCIE1);
-//     PCMSK1 |= (1 << PCINT10);
-// }
+void checkMenuScroll() {
+    int en1;
+    int en2;
+    unsigned long timestamp;
 
-// volatile int encoder1 = HIGH;
-// volatile int encoder2 = HIGH;
-// volatile unsigned long encoderTimestamp = 0UL;
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        en1 = encoder1;
+        en2 = encoder2;
+        timestamp = encoderTimestamp;
+    }
 
-// ISR(PCINT1_vect)
-// {
-//     encoder1 = digitalRead(ENCODER1);
-//     encoder2 = digitalRead(ENCODER2);
-//     encoderTimestamp = millis();
-// }
+    if (en1 == LOW && timestamp > lastChangeTimestamp + BUTTON_DEBOUNCE_PERIOD)
+    {
+        if (en2 == HIGH)
+        {
+            menuVisibleItem = (menuVisibleItem + 1) % numberOfMenuOptions;
+        }
+        else
+        {
+            menuVisibleItem = (menuVisibleItem - 1 < 0) ? numberOfMenuOptions - 1 : menuVisibleItem - 1;
+        }
+        printMenu();
+        lastChangeTimestamp = timestamp;
+    }
+    lastEn1 = en1;
+}
 
-// int encoderValue = 0;
-// int lastEn1 = LOW;
-// unsigned long lastChangeTimestamp = 0UL;
+#define ENCODER_DEBOUNCE_PERIOD 10UL
+int buttons[] = {GREEN_BUTTON, RED_BUTTON};
+int debouncedStates[] = {HIGH, HIGH};
+int previousReadings[] = {HIGH, HIGH};
+unsigned long lastChanged[] = {0UL, 0UL};
 
-// void checkMenuScroll() {
-//     int en1;
-//     int en2;
-//     unsigned long timestamp;
+bool isButtonPressed(int buttonIdx) {
+    bool isPressed = false;
+    int current_reading = digitalRead(buttons[buttonIdx]);
 
-//     ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-//     {
-//         en1 = encoder1;
-//         en2 = encoder2;
-//         timestamp = encoderTimestamp;
-//     }
+    if (previousReadings[buttonIdx] != current_reading)
+    {
+        lastChanged[buttonIdx] = millis();
+    }
 
-//     if (en1 == LOW && timestamp > lastChangeTimestamp + BUTTON_DEBOUNCE_PERIOD)
-//     {
-//         if (en2 == HIGH)
-//         {
-//             menuTopItem = (menuTopItem + 1) % numberOfMenuOptions;
-//         }
-//         else
-//         {
-//             menuTopItem = (menuTopItem - 1 < 0) ? numberOfMenuOptions - 1 : menuTopItem - 1;
-//         }
-//         printMenu();
-//         lastChangeTimestamp = timestamp;
-//     }
-//     lastEn1 = en1;
-// }
+    if (millis() - lastChanged[buttonIdx] > ENCODER_DEBOUNCE_PERIOD)
+    {
+        if(current_reading != debouncedStates[buttonIdx])
+        {
+            if (debouncedStates[buttonIdx] == HIGH && current_reading == LOW)
+            {
+                isPressed = true;
+            }
+            debouncedStates[buttonIdx] = current_reading;
+        }
+    }
 
-// #define ENCODER_DEBOUNCE_PERIOD 10UL
-// int buttons[] = {GREEN_BUTTON, RED_BUTTON};
-// int debouncedStates[] = {HIGH, HIGH};
-// int previousReadings[] = {HIGH, HIGH};
-// unsigned long lastChanged[] = {0UL, 0UL};
+    previousReadings[buttonIdx] = current_reading;
+    return isPressed;
+}
 
-// bool isButtonPressed() {
-//     int greenButtonId = 0;
+void printColorIntensityPercent(String color, int val)
+{
+    char buffer[40];
+    lcd.setCursor(0, 0);
+    lcd.print(color);
+    lcd.setCursor(0, 1);
+    sprintf(buffer, "Percent: %3d", getPercentOfIntensity(val));
+    lcd.print(buffer);
+}
 
-//     bool isPressed = false;
-//     int current_reading = digitalRead(buttons[greenButtonId]);
+bool isSettingIntensity = false;
 
-//     if (previousReadings[greenButtonId] != current_reading)
-//     {
-//         lastChanged[greenButtonId] = millis();
-//     }
+int intensityValue = 0;
+int lastEn1Intensity = LOW;
+unsigned long lastIntensityChangeTimestamp = 0UL;
 
-//     if (millis() - lastChanged[greenButtonId] > ENCODER_DEBOUNCE_PERIOD)
-//     {
-//         if(current_reading != debouncedStates[greenButtonId])
-//         {
-//             if (debouncedStates[greenButtonId] == HIGH && current_reading == LOW)
-//             {
-//                 isPressed = true;
-//             }
-//             debouncedStates[greenButtonId] = current_reading;
-//         }
-//     }
+void setColorIntensity(int colorIdx) {
+  lcd.clear();
+  printColorIntensityPercent(getColorName(colorIdx), colorIntensities[colorIdx]);
+  isSettingIntensity = true;
 
-//     previousReadings[greenButtonId] = current_reading;
-//     return isPressed;
-// }
+  while (!isButtonPressed(1)) {
+    int en1;
+    int en2;
+    unsigned long timestamp;
 
-// void performMenuOption() {
-//   switch (menuTopItem) {
-//     case 0:
-//       digitalWrite(LED_RED, HIGH);
-//       digitalWrite(LED_GREEN, LOW);
-//       digitalWrite(LED_BLUE, LOW);
-//       break;
-//     case 1:
-//       digitalWrite(LED_RED, LOW);
-//       digitalWrite(LED_GREEN, HIGH);
-//       digitalWrite(LED_BLUE, LOW);
-//       break;
-//     case 2:
-//       digitalWrite(LED_RED, LOW);
-//       digitalWrite(LED_GREEN, LOW);
-//       digitalWrite(LED_BLUE, HIGH);
-//       break;
-//     case 3:
-//       digitalWrite(LED_RED, HIGH);
-//       digitalWrite(LED_GREEN, HIGH);
-//       digitalWrite(LED_BLUE, HIGH);
-//       break;
-//     case 4:
-//       digitalWrite(LED_RED, LOW);
-//       digitalWrite(LED_GREEN, LOW);
-//       digitalWrite(LED_BLUE, LOW);
-//       break;
-//     default:
-//       break;
-//   }
-// }
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        en1 = encoder1;
+        en2 = encoder2;
+        timestamp = encoderTimestamp;
+    }
 
-// void loop()
-// {
-//   checkMenuScroll();
-//   if (isButtonPressed())
-//     performMenuOption();
-// }
+    if (en1 == LOW && lastEn1 == HIGH && timestamp > lastIntensityChangeTimestamp + DEBOUNCING_PERIOD)
+    {
+        if (en2 == HIGH)
+        {
+            if (colorIntensities[colorIdx] < 255)
+                colorIntensities[colorIdx] += 15;
+        }
+        else
+        {
+            if (colorIntensities[colorIdx] > 0)
+                colorIntensities[colorIdx] -= 15;
+        }
+        lastIntensityChangeTimestamp = timestamp;
+
+        printColorIntensityPercent(getColorName(colorIdx), colorIntensities[colorIdx]);
+        analogWrite(colors[colorIdx], colorIntensities[colorIdx]);
+    }
+    lastEn1Intensity = en1;
+  }
+}
+
+void goBackToMenu() {
+    isScrollingMenu = true;
+    lcd.clear();
+    printMenu();
+}
+
+void switchToChosenOption() {
+  switch (menuVisibleItem) {
+    case 0:
+      setColorIntensity(0);
+      break;
+    case 1:
+      setColorIntensity(1);
+      break;
+    case 2:
+      setColorIntensity(2);
+      break;
+    default:
+      break;
+  }
+  goBackToMenu();
+}
+
+void setup()
+{
+    initButtons();
+    initRGB();
+    initEncoder();
+    initLCD();
+
+    printMenu();
+
+    PCICR |= (1 << PCIE1);
+    PCMSK1 |= (1 << PCINT10);
+}
+
+void loop()
+{
+  if (isScrollingMenu)
+    checkMenuScroll();
+  if (isScrollingMenu && isButtonPressed(0)) {
+    isScrollingMenu = false;
+    switchToChosenOption();
+  }
+}
