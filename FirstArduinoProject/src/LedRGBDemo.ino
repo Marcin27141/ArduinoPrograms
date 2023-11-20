6460a1bc017c9e264bafbbd643a7082e81d477bc
Marcin27141 <marcin.tkocz@op.pl>
2023-11-20 14:23:54 +0100

    added display for extreme temperatures

--- FirstArduinoProject/src/LedRGBDemo.ino
+++ FirstArduinoProject/src/LedRGBDemo.ino
@@ -1,130 +1,130 @@
-#include <Arduino.h>
-#include <LedRGB.h>
+// #include <Arduino.h>
+// #include <LedRGB.h>
 
-#define LED_RED 6
-#define LED_GREEN 5
-#define LED_BLUE 3
+// #define LED_RED 6
+// #define LED_GREEN 5
+// #define LED_BLUE 3
 
-LedRGB ledRgb(LED_RED, LED_GREEN, LED_BLUE);
+// LedRGB ledRgb(LED_RED, LED_GREEN, LED_BLUE);
 
-bool isValidNumber(String input) {
-  int result = input.toInt();
-  return input == "0" || (result > 0 && result <= 255);
-}
+// bool isValidNumber(String input) {
+//   int result = input.toInt();
+//   return input == "0" || (result > 0 && result <= 255);
+// }
 
-bool validatePassedValues(String stringParams[], int length) {
-    String notValidMessage = " is not a valid number. Put a number from 0 to 255";
+// bool validatePassedValues(String stringParams[], int length) {
+//     String notValidMessage = " is not a valid number. Put a number from 0 to 255";
 
-    for (int i = 0; i < length; i++) {
-        if (!isValidNumber(stringParams[i])) {
-            Serial.println(stringParams[i] + notValidMessage);
-            return false;
-        }
-    }
+//     for (int i = 0; i < length; i++) {
+//         if (!isValidNumber(stringParams[i])) {
+//             Serial.println(stringParams[i] + notValidMessage);
+//             return false;
+//         }
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
-void printSuccessfulCommand(String params[], int length) {
-    for (int i = 0; i < length; i++) {
-        Serial.print(params[i]);
-        Serial.print(" ");
-    }
-    Serial.println();
-}
+// void printSuccessfulCommand(String params[], int length) {
+//     for (int i = 0; i < length; i++) {
+//         Serial.print(params[i]);
+//         Serial.print(" ");
+//     }
+//     Serial.println();
+// }
 
-void lightLeds(String paramStrings[]) {
-    int r_intensity = paramStrings[0].toInt();
-    int g_intensity = paramStrings[1].toInt();
-    int b_intensity = paramStrings[2].toInt();
+// void lightLeds(String paramStrings[]) {
+//     int r_intensity = paramStrings[0].toInt();
+//     int g_intensity = paramStrings[1].toInt();
+//     int b_intensity = paramStrings[2].toInt();
 
-    analogWrite(LED_RED, r_intensity);
-    analogWrite(LED_GREEN, g_intensity);
-    analogWrite(LED_BLUE, b_intensity);
-}
+//     analogWrite(LED_RED, r_intensity);
+//     analogWrite(LED_GREEN, g_intensity);
+//     analogWrite(LED_BLUE, b_intensity);
+// }
 
 
-const int VALID_NUMBER_OF_RGB_PARAMETERS = 3;
+// const int VALID_NUMBER_OF_RGB_PARAMETERS = 3;
 
-void processRgbCommand(String command) {
-    int space1 = command.indexOf(' ');
-    int space2 = command.indexOf(' ', space1 + 1);
-    int space3 = command.indexOf(' ', space2 + 1);
+// void processRgbCommand(String command) {
+//     int space1 = command.indexOf(' ');
+//     int space2 = command.indexOf(' ', space1 + 1);
+//     int space3 = command.indexOf(' ', space2 + 1);
 
-    if (space1 == -1 || space2 == -1 || space3 != -1) {
-        Serial.println("Incorrect line format. Separate 3 intensity values with single spaces");
-    }
-    else {
-        String paramStrings[] = {
-            command.substring(0, space1),
-            command.substring(space1 + 1, space2),
-            command.substring(space2 + 1)
-        };
+//     if (space1 == -1 || space2 == -1 || space3 != -1) {
+//         Serial.println("Incorrect line format. Separate 3 intensity values with single spaces");
+//     }
+//     else {
+//         String paramStrings[] = {
+//             command.substring(0, space1),
+//             command.substring(space1 + 1, space2),
+//             command.substring(space2 + 1)
+//         };
 
-        bool valuesAreValid = validatePassedValues(paramStrings, VALID_NUMBER_OF_RGB_PARAMETERS);
-        if (valuesAreValid) {
-            ledRgb.lightRgb(paramStrings[0].toInt(), paramStrings[1].toInt(), paramStrings[2].toInt());
-            printSuccessfulCommand(paramStrings, VALID_NUMBER_OF_RGB_PARAMETERS);
-        }
-    }
-}
+//         bool valuesAreValid = validatePassedValues(paramStrings, VALID_NUMBER_OF_RGB_PARAMETERS);
+//         if (valuesAreValid) {
+//             ledRgb.lightRgb(paramStrings[0].toInt(), paramStrings[1].toInt(), paramStrings[2].toInt());
+//             printSuccessfulCommand(paramStrings, VALID_NUMBER_OF_RGB_PARAMETERS);
+//         }
+//     }
+// }
 
-void processColorCommand(String command) {
-    int color;
-    bool wasFound = true;
-    if (command.equals("red"))
-        color = RED;
-    else if (command.equals("green"))
-        color = GREEN;
-    else if (command.equals("blue"))
-        color = BLUE;
-    else if (command.equals("yellow"))
-        color = YELLOW;
-    else if (command.equals("cyan"))
-        color = CYAN;
-    else if (command.equals("magenta"))
-        color = MAGENTA;
-    else if (command.equals("black"))
-        color = BLACK;
-    else if (command.equals("white"))
-        color = WHITE;
-    else {
-        wasFound = false;
-        Serial.println("Color was not found");
-    }
+// void processColorCommand(String command) {
+//     int color;
+//     bool wasFound = true;
+//     if (command.equals("red"))
+//         color = RED;
+//     else if (command.equals("green"))
+//         color = GREEN;
+//     else if (command.equals("blue"))
+//         color = BLUE;
+//     else if (command.equals("yellow"))
+//         color = YELLOW;
+//     else if (command.equals("cyan"))
+//         color = CYAN;
+//     else if (command.equals("magenta"))
+//         color = MAGENTA;
+//     else if (command.equals("black"))
+//         color = BLACK;
+//     else if (command.equals("white"))
+//         color = WHITE;
+//     else {
+//         wasFound = false;
+//         Serial.println("Color was not found");
+//     }
         
 
-    if (wasFound)
-        ledRgb.lightPredefinedColor(color);
-}
+//     if (wasFound)
+//         ledRgb.lightPredefinedColor(color);
+// }
 
-//message format: 'rgb:red green blue'
-//or: 'color:colorName'
-void processSerialCommand() {
-    String command = Serial.readStringUntil('\n');
-    command.trim();
+// //message format: 'rgb:red green blue'
+// //or: 'color:colorName'
+// void processSerialCommand() {
+//     String command = Serial.readStringUntil('\n');
+//     command.trim();
 
-    if (command.startsWith("color:"))
-        processColorCommand(command.substring(6));
-    else if (command.startsWith("rgb:"))
-        processRgbCommand(command.substring(4));
-    else
-        Serial.println("Unrecognized command. Start with 'color:' or 'rgb:'");
-}
+//     if (command.startsWith("color:"))
+//         processColorCommand(command.substring(6));
+//     else if (command.startsWith("rgb:"))
+//         processRgbCommand(command.substring(4));
+//     else
+//         Serial.println("Unrecognized command. Start with 'color:' or 'rgb:'");
+// }
 
-void setup()
-{
-    Serial.begin(9600);
-    while (!Serial)
-    {
-    }
-    ledRgb.turnOn();
-}
+// void setup()
+// {
+//     Serial.begin(9600);
+//     while (!Serial)
+//     {
+//     }
+//     ledRgb.turnOn();
+// }
 
-void loop()
-{
-    if (Serial.available() > 0)
-    {
-        processSerialCommand();
-    }
-}
+// void loop()
+// {
+//     if (Serial.available() > 0)
+//     {
+//         processSerialCommand();
+//     }
+// }
--- FirstArduinoProject/src/labList6.ino
+++ FirstArduinoProject/src/labList6.ino
@@ -1,149 +1,222 @@
-// #include <Arduino.h>
-// #include <LiquidCrystal_I2C.h>
-// #include <OneWire.h>
-// #include <DallasTemperature.h>
-// #include <Initializer.h>
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Initializer.h>
 
-// LiquidCrystal_I2C lcd(0x27,16,2);
-// #define LCD_ROW_INSIDE 0
-// #define LCD_ROW_OUTSIDE 1
+LiquidCrystal_I2C lcd(0x27,16,2);
+#define LCD_ROW_INSIDE 0
+#define LCD_ROW_OUTSIDE 1
 
-// // #define LED_RED 6
-// // #define LED_GREEN 5
-// // #define LED_BLUE 3
-
-// #define ONE_WIRE A1
-// OneWire oneWire(ONE_WIRE);
-// DallasTemperature sensors(&oneWire);
-
-// DeviceAddress mySensor = { 0x10, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xFB };
-// #define MAIN_SENSOR_IDX 0
+// #define LED_RED 6
+// #define LED_GREEN 5
+// #define LED_BLUE 3
+// #define RED_BUTTON 2
+#define DEBOUNCING_PERIOD 100
+bool isShowingTemperatures = true;
 
 
-// #define MAX_TEMP 125
-// #define LOWER_DECENT_TEMP 19
-// #define UPPER_DECENT_TEMP 26
+#define ONE_WIRE A1
+OneWire oneWire(ONE_WIRE);
+DallasTemperature sensors(&oneWire);
+unsigned long lastSensorReading = 0UL;
+int const READINGS_WINDOW = 1000;
 
-// float minTemp;
-// float maxTemp;
+DeviceAddress mySensor = { 0x10, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xFB };
+#define MAIN_SENSOR_IDX 0
 
-// void initLCD()
-// {
-//   lcd.init();
-//   lcd.clear();
-//   lcd.backlight();
-// }
 
-// void initRGB()
-// {
-//   pinMode(LED_RED, OUTPUT);
-//   pinMode(LED_GREEN, OUTPUT);
-//   pinMode(LED_BLUE, OUTPUT);
-// }
+#define MAX_TEMP 125
+#define LOWER_DECENT_TEMP 19
+#define UPPER_DECENT_TEMP 26
 
-// void printSerialNumber(int sensorIdx) {
-//     DeviceAddress tempDeviceAddress;
-//     sensors.getAddress(tempDeviceAddress, sensorIdx);
+float minTemp;
+float maxTemp;
 
-//     Serial.print("Serial number: ");
-//     for (uint8_t j = 0; j < 8; j++) {
-//       Serial.print(tempDeviceAddress[j], HEX);
-//       Serial.print(" ");
-//     }
-//     Serial.println();
-// }
+void initLCD()
+{
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+}
 
-// float getFilteredTempValue(float reading) {
-//     if (reading > MAX_TEMP) return 0;
-//     return reading;
-// }
+void initRGB()
+{
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+}
 
-// void identifySensorsByTemp() {
-//   sensors.requestTemperatures();
-//   float temp0 = sensors.getTempCByIndex(0);
-//   float temp1 = sensors.getTempCByIndex(1);
+volatile unsigned long buttonTimestamp = 0UL;
+unsigned long previousButtonTimestamp = 0UL;
+unsigned long elapsedStartTimestamp = 0UL;
 
-//   Serial.print("Sensor 0: ");
-//   printSerialNumber(0);
-//   Serial.print("Temperature: ");
-//   Serial.print(temp0);
-//   Serial.println(" Celsius");
+void interruptAction()
+{
+    buttonTimestamp = millis();
+}
 
-//   Serial.print("Sensor 1: ");
-//   printSerialNumber(1);
-//   Serial.print("Temperature: ");
-//   Serial.print(temp1);
-//   Serial.println(" Celsius");
-// }
+bool redButtonIsPressed() {
+    bool result = false;
+    noInterrupts();
+    unsigned long localButtonTimestamp = buttonTimestamp;
+    interrupts();
 
-// void printInitialLCD() {
-//     lcd.setCursor(0, 0);
-//     lcd.print("Inside:");
-//     lcd.setCursor(0,1);
-//     lcd.print("Outside:");
-// }
+    if (localButtonTimestamp != previousButtonTimestamp && millis() > localButtonTimestamp + DEBOUNCING_PERIOD)
+    {
+        if (digitalRead(RED_BUTTON) == LOW)
+        {
+            result = true;
 
-// void turnOffLED() {
-//     digitalWrite(LED_RED, LOW);
-//     digitalWrite(LED_GREEN, LOW);
-//     digitalWrite(LED_BLUE, LOW);
-// }
+            unsigned long elapsedTime = localButtonTimestamp - elapsedStartTimestamp;
+            elapsedStartTimestamp = localButtonTimestamp;
+        }
+        previousButtonTimestamp = localButtonTimestamp;
+    }
 
-// void showTempOnLED(float temp) {
-//     turnOffLED();
-//     if (temp < LOWER_DECENT_TEMP)
-//         digitalWrite(LED_BLUE, HIGH);
-//     else if (temp > UPPER_DECENT_TEMP)
-//         digitalWrite(LED_RED, HIGH);
-//     else
-//         digitalWrite(LED_GREEN, HIGH);
-// }
+    return result;
+}
 
-// void printTempOnLcd(int row, float temp) {
-//     // lcd.setCursor(8, row);
-//     // char buffer[40];
-//     // sprintf(buffer, "%8s", String(temp, 4).c_str());
-//     lcd.setCursor(10, row);
-//     char buffer[5];
-//     sprintf(buffer, "%5s", String(temp, 1).c_str());
-//     lcd.print(buffer);
-//     lcd.print("C");
-// }
+void printSerialNumber(int sensorIdx) {
+    DeviceAddress tempDeviceAddress;
+    sensors.getAddress(tempDeviceAddress, sensorIdx);
 
-// void readInitialTemp() {
-//     sensors.requestTemperatures();
-//     float tempOutside = sensors.getTempCByIndex(MAIN_SENSOR_IDX);
-//     tempOutside = getFilteredTempValue(tempOutside);
-//     minTemp = tempOutside;
-//     maxTemp = tempOutside;
-// }
+    Serial.print("Serial number: ");
+    for (uint8_t j = 0; j < 8; j++) {
+      Serial.print(tempDeviceAddress[j], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+}
 
-// void noteIfExtremeValue(float temp) {
-//     if (temp < minTemp)
-//         minTemp = temp;
-//     else if (temp > maxTemp)
-//         maxTemp = temp;
-// }
+float getFilteredTempValue(float reading) {
+    if (reading > MAX_TEMP) return 0;
+    return reading;
+}
 
-// void setup()
-// {
-//     sensors.begin();
-//     initLCD();
+void identifySensorsByTemp() {
+  sensors.requestTemperatures();
+  float temp0 = sensors.getTempCByIndex(0);
+  float temp1 = sensors.getTempCByIndex(1);
 
-//     readInitialTemp();
-//     printInitialLCD();
-// }
+  Serial.print("Sensor 0: ");
+  printSerialNumber(0);
+  Serial.print("Temperature: ");
+  Serial.print(temp0);
+  Serial.println(" Celsius");
 
-// void loop()
-// {
-//   sensors.requestTemperatures();
-//   float tempOutside = sensors.getTempCByIndex(MAIN_SENSOR_IDX);
-//   tempOutside = getFilteredTempValue(tempOutside);
-//   noteIfExtremeValue(tempOutside);
-//   //float temperatureFahrenheit = sensors.toFahrenheit(temperatureCelsius);
+  Serial.print("Sensor 1: ");
+  printSerialNumber(1);
+  Serial.print("Temperature: ");
+  Serial.print(temp1);
+  Serial.println(" Celsius");
+}
 
-//   printTempOnLcd(LCD_ROW_INSIDE, 17.4);
-//   printTempOnLcd(LCD_ROW_OUTSIDE, tempOutside);
-//   showTempOnLED(tempOutside);
-//   delay(1000);
-// }
+void printInitialLCD() {
+    lcd.setCursor(0, 0);
+    lcd.print("Inside:");
+    lcd.setCursor(0,1);
+    lcd.print("Outside:");
+}
+
+void turnOffLED() {
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_BLUE, LOW);
+}
+
+void showTempOnLED(float temp) {
+    turnOffLED();
+    if (temp < LOWER_DECENT_TEMP)
+        digitalWrite(LED_BLUE, HIGH);
+    else if (temp > UPPER_DECENT_TEMP)
+        digitalWrite(LED_RED, HIGH);
+    else
+        digitalWrite(LED_GREEN, HIGH);
+}
+
+void printExtremeTemperaturesOnLcd() {
+    lcd.setCursor(0,0);
+    lcd.print("Max:    ");
+
+    lcd.setCursor(10, 0);
+    char bufferMax[5];
+    sprintf(bufferMax, "%5s", String(maxTemp, 1).c_str());
+    lcd.print(bufferMax);
+    lcd.print("C");
+
+    lcd.setCursor(0,1);
+    lcd.print("Min:    ");
+
+    lcd.setCursor(10, 1);
+    char bufferMin[5];
+    sprintf(bufferMin, "%5s", String(minTemp, 1).c_str());
+    lcd.print(bufferMin);
+    lcd.print("C");
+}
+
+void printTempOnLcd(int row, float temp) {
+    // lcd.setCursor(8, row);
+    // char buffer[40];
+    // sprintf(buffer, "%8s", String(temp, 4).c_str());
+    lcd.setCursor(10, row);
+    char buffer[5];
+    sprintf(buffer, "%5s", String(temp, 1).c_str());
+    lcd.print(buffer);
+    lcd.print("C");
+}
+
+void readInitialTemp() {
+    sensors.requestTemperatures();
+    float tempOutside = sensors.getTempCByIndex(MAIN_SENSOR_IDX);
+    tempOutside = getFilteredTempValue(tempOutside);
+    minTemp = tempOutside;
+    maxTemp = tempOutside;
+}
+
+void noteIfExtremeValue(float temp) {
+    if (temp < minTemp)
+        minTemp = temp;
+    else if (temp > maxTemp)
+        maxTemp = temp;
+}
+
+void setup()
+{
+    pinMode(RED_BUTTON, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(RED_BUTTON), interruptAction, FALLING);
+    sensors.begin();
+    initLCD();
+
+    readInitialTemp();
+    printInitialLCD();
+}
+
+void switchMenus() {
+    if (isShowingTemperatures) {
+        isShowingTemperatures = false;
+        printExtremeTemperaturesOnLcd();
+    } else {
+        isShowingTemperatures = true;
+        printInitialLCD();
+    }
+}
+
+void loop()
+{
+    if (redButtonIsPressed())
+        switchMenus();
+    if (isShowingTemperatures && millis() - lastSensorReading > READINGS_WINDOW) {
+        lastSensorReading = millis();
+        sensors.requestTemperatures();
+        float tempOutside = sensors.getTempCByIndex(MAIN_SENSOR_IDX);
+        tempOutside = getFilteredTempValue(tempOutside);
+        noteIfExtremeValue(tempOutside);
+        //float temperatureFahrenheit = sensors.toFahrenheit(temperatureCelsius);
+
+        printTempOnLcd(LCD_ROW_INSIDE, 17.4);
+        printTempOnLcd(LCD_ROW_OUTSIDE, tempOutside);
+        showTempOnLED(tempOutside);
+    }
+    
+}
